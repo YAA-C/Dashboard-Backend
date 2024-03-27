26a733abf81a223ce02764bed3ea25b990b81795
Shubham Sukum <shubhamsukum1722002@gmail.com>
2024-03-27 23:53:28 +0530

    create match api for ICU

--- src/models/matches.js
+++ src/models/matches.js
@@ -18,6 +18,68 @@ const histSchema = {
   default: null,
 };
 
+const Charts = {
+  report_1: barSchema,
+  report_2: barSchema,
+  report_3: pieSchema,
+  report_4: pieSchema,
+  report_5: {
+    weapon_category_ar: {
+      weapon_ak47: pieSchema,
+      weapon_aug: pieSchema,
+      weapon_famas: pieSchema,
+      weapon_galilar: pieSchema,
+      weapon_m4a1_silencer: pieSchema,
+      weapon_m4a1: pieSchema,
+      weapon_sg556: pieSchema,
+    },
+    weapon_category_smg: {
+      weapon_bizon: pieSchema,
+      weapon_mac10: pieSchema,
+      weapon_mp5sd: pieSchema,
+      weapon_mp7: pieSchema,
+      weapon_mp9: pieSchema,
+      weapon_ump45: pieSchema,
+      weapon_p90: pieSchema,
+    },
+    weapon_category_sniper: {
+      weapon_awp: pieSchema,
+      weapon_ssg08: pieSchema,
+      weapon_g3sg1: pieSchema,
+      weapon_scar20: pieSchema,
+    },
+    weapon_category_lmg: {
+      weapon_m249: pieSchema,
+      weapon_negev: pieSchema,
+    },
+    weapon_category_pistol: {
+      weapon_deagle: pieSchema,
+      weapon_elite: pieSchema,
+      weapon_fiveseven: pieSchema,
+      weapon_glock: pieSchema,
+      weapon_hkp2000: pieSchema,
+      weapon_usp_silencer: pieSchema,
+      weapon_cz75a: pieSchema,
+      weapon_p250: pieSchema,
+      weapon_tec9: pieSchema,
+    },
+    weapon_category_shotgun: {
+      weapon_mag7: pieSchema,
+      weapon_nova: pieSchema,
+      weapon_xm1014: pieSchema,
+      weapon_sawedoff: pieSchema,
+    },
+  },
+  report_6: pieSchema,
+  report_7: histSchema,
+  report_8: barSchema,
+  report_9: {
+    weapon_ar: pieSchema,
+    weapon_sniper: pieSchema,
+  },
+  report_10: pieSchema,
+};
+
 const matchesSchema = new mongoose.Schema(
   {
     account_id: {
@@ -26,65 +88,8 @@ const matchesSchema = new mongoose.Schema(
       required: true,
     },
     charts: {
-      report_1: barSchema,
-      report_2: barSchema,
-      report_3: pieSchema,
-      report_4: pieSchema,
-      report_5: {
-        weapon_category_ar: {
-          weapon_ak47: pieSchema,
-          weapon_aug: pieSchema,
-          weapon_famas: pieSchema,
-          weapon_galilar: pieSchema,
-          weapon_m4a1_silencer: pieSchema,
-          weapon_m4a1: pieSchema,
-          weapon_sg556: pieSchema,
-        },
-        weapon_category_smg: {
-          weapon_bizon: pieSchema,
-          weapon_mac10: pieSchema,
-          weapon_mp5sd: pieSchema,
-          weapon_mp7: pieSchema,
-          weapon_mp9: pieSchema,
-          weapon_ump45: pieSchema,
-          weapon_p90: pieSchema,
-        },
-        weapon_category_sniper: {
-          weapon_awp: pieSchema,
-          weapon_ssg08: pieSchema,
-          weapon_g3sg1: pieSchema,
-          weapon_scar20: pieSchema,
-        },
-        weapon_category_lmg: {
-          weapon_m249: pieSchema,
-          weapon_negev: pieSchema,
-        },
-        weapon_category_pistol: {
-          weapon_deagle: pieSchema,
-          weapon_elite: pieSchema,
-          weapon_fiveseven: pieSchema,
-          weapon_glock: pieSchema,
-          weapon_hkp2000: pieSchema,
-          weapon_usp_silencer: pieSchema,
-          weapon_cz75a: pieSchema,
-          weapon_p250: pieSchema,
-          weapon_tec9: pieSchema,
-        },
-        weapon_category_shotgun: {
-          weapon_mag7: pieSchema,
-          weapon_nova: pieSchema,
-          weapon_xm1014: pieSchema,
-          weapon_sawedoff: pieSchema,
-        },
-      },
-      report_6: pieSchema,
-      report_7: histSchema,
-      report_8: barSchema,
-      report_9: {
-        weapon_ar: pieSchema,
-        weapon_sniper: pieSchema,
-      },
-      report_10: pieSchema,
+      type: Charts,
+      default: null,
     },
     is_Analyzed: {
       type: Boolean,
@@ -94,20 +99,23 @@ const matchesSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    players: [
-      {
+    players: {
+      type: [{
         steamid: {
           type: String,
+          default: null
         },
         playerName: {
           type: String,
+          default: null
         },
         isCheating: {
           type: Boolean,
           default: false,
         },
-      },
-    ],
+      }],
+      default: []
+    },
   },
   {
     versionKey: false,
--- src/routes/matchesRoute.js
+++ src/routes/matchesRoute.js
@@ -1,35 +1,72 @@
 import express from "express";
 import multer from "multer";
+
 import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3Client } from "../configs/s3ConfigTebi.js";
+
+import { userModel } from "../models/userModel.js";
+import { matchesModel } from "../models/matches.js";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 export const matchesRouter = express.Router();
 
-matchesRouter.post("/create", upload.single("csvFile"), async (req, res) => {
-  try {
-    const file = req.file;
+matchesRouter.post("/create/:apikey",upload.single("csvFile"),async (req, res) => {
+    try {
+      const file = req.file;
+      const { apikey } = req.params;
 
-    const upload_data = await s3Client.send(
-      new PutObjectCommand({
+      const user = await userModel.findOne({ apikey });
+
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found. Please sign up.",
+          success: false,
+        });
+      }
+
+      const upload_data = await s3Client.send(
+        new PutObjectCommand({
+          Bucket: "yaacs",
+          Key: file.originalname,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        })
+      );
+
+      const get_command = new GetObjectCommand({
         Bucket: "yaacs",
         Key: file.originalname,
-        Body: file.buffer,
-        ContentType: file.mimetype,
-      })
-    );
+      });
 
-    res
-      .status(200)
-      .json({
-        message: "File uploaded successfully",
-        data: upload_data,
+      const url = await getSignedUrl(s3Client, get_command, {
+        expiresIn: 3600,
+      });
+
+      const matchData = {
+        account_id: user._id,
+        tebi_Link: url,
+      };
+
+      matchesModel
+        .create(matchData)
+        .then((savedDocument) => {
+          console.log("Document saved successfully:", savedDocument._id);
+        })
+        .catch((error) => {
+          console.error("Error saving document:", error);
+        });
+
+      res.status(200).json({
         success: true,
       });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error", success: false });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "Internal server error", success: false });
+    }
   }
-});
+);
